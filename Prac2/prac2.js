591ab06b519e72d4e93c1e460c708fc098e755c3
DianaRomIP13 <90607027+DianaRomIP13@users.noreply.github.com>
2023-06-01 00:14:42 +0300

    Update prac2.js

--- Prac2/prac2.js
+++ Prac2/prac2.js
@@ -13,18 +13,22 @@ function add(num) {
 console.log('task1 answer for 2,5,7,1,6,5,11: ', add(2)(5)(7)(1)(6)(5)(11)());
 //task2
 function is_anagram(str1, str2) {
-    var arr1 = str1.split('').sort();
-    var arr2 = str2.split('').sort();
-    if (arr1.length !== arr2.length) {
+    if (str1.length !== str2.length) {
         return false;
     }
-    for (var i = 0; i < arr1.length; i++) {
+  
+    let arr1 = str1.split('').sort();
+    let arr2 = str2.split('').sort();
+  
+    for (let i = 0; i < arr1.length; i++) {
         if (arr1[i] !== arr2[i]) {
             return false;
         }
     }
+  
     return true;
 }
+
 console.log('task2 answer for true: ', is_anagram('слоник', 'кислон'));
 console.log('task2 answer for false:', is_anagram('слоник', 'бджілка'));
 //task3
@@ -32,37 +36,37 @@ function clone(obj) {
     if (obj === null || typeof obj !== "object") {
         return obj;
     }
-    var cloned = Array.isArray(obj) ? [] : {};
+    let cloned = Array.isArray(obj) ? [] : {};
     for (var key in obj) {
         cloned[key] = clone(obj[key]);
     }
     return cloned;
 }
-var obj = {
+let obj = {
     name: "slonik",
     age: 12,
 };
-var cloned = clone(obj);
+let cloned = clone(obj);
 console.log("task3 answer: ", obj === cloned);
 //task4
 function wrping(fn) {
-    var cache = {};
+    const cache = new Map();
     return function () {
-        var args = [];
-        for (var _i = 0; _i < arguments.length; _i++) {
-            args[_i] = arguments[_i];
+        const args = Array.from(arguments);
+        const key = JSON.stringify(args); // unique keys
+        if (cache.has(key)) {
+            return cache.get(key);
         }
-        var key = JSON.stringify(args); // unique keys
-        if (key in cache) {
-            return cache[key];
-        }
-        var result = fn.apply(this, args);
-        cache[key] = result;
+        const result = fn.apply(this, args);
+        cache.set(key, result);
         return result;
     };
 }
+
 function sum(a, b, c) {
     return a + b + c;
 }
-var wrappedAdd = wrping(sum);
+
+const wrappedAdd = wrping(sum);
 console.log('task4 answer for 2, 2, 3: ', wrappedAdd(2, 2, 3));
+
